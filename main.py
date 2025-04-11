1c308953c8918088bc3e55a634eaa7a8659c4c5e
bypschroeder <bypschroeder@gmail.com>
2025-04-11 14:11:13 +0200

    init

---
+++ main.py
@@ -0,0 +1,39 @@
+import cv2
+import os
+import numpy as np
+import svgwrite
+
+image_path = os.path.abspath("./images/02.png")
+outline_path = "output/outline.png"
+svg_path = "output/vector.svg"
+
+img = cv2.imread(image_path)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Rauschunterdrückung ohne Detailverlust
+smooth = cv2.bilateralFilter(gray, d=9, sigmaColor=75, sigmaSpace=75)
+
+# Canny: zwei Varianten mit unterschiedlicher Empfindlichkeit
+edges_strong = cv2.Canny(smooth, 100, 200)  # klare Hauptkanten
+edges_soft = cv2.Canny(smooth, 30, 100)  # feinere Kanten
+
+# Kanten zusammenführen
+combined_edges = cv2.bitwise_or(edges_strong, edges_soft)
+
+# Optional: Lücken glätten
+kernel = np.ones((2, 2), np.uint8)
+combined_edges = cv2.dilate(combined_edges, kernel, iterations=1)
+combined_edges = cv2.erode(combined_edges, kernel, iterations=1)
+
+cv2.imwrite(outline_path, combined_edges)
+
+contours, _ = cv2.findContours(combined_edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+height, width = combined_edges.shape
+dwg = svgwrite.Drawing(svg_path, size=(width, height))
+
+for contour in contours:
+    path_data = "M " + " L ".join(f"{pt[0][0]},{pt[0][1]}" for pt in contour) + " Z"
+    dwg.add(dwg.path(d=path_data, fill="none", stroke="black", stroke_width=4))
+
+dwg.save()
